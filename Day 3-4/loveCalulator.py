e4ea73e2724c37893eb8cbbb89d16a82bb80d8e3
Hi, I'mAmy! <afelix51228@gmail.com>
2022-05-09 15:30:04 -0600

    added a new folder and new projects

--- Day 3-4/loveCalulator.py
+++ Day 3-4/loveCalulator.py
@@ -4,16 +4,27 @@ name1 = input("What is your name? \n")
 name2 = input("What is their name? \n")
 # 🚨 Don't change the code above 👆
 
-#Write your code below this line 👇
-lowerName = name1.lower()
-countName = lowerName.count()
+# Write your code below this line 👇
+combinedNames = name1 + name2
+lowerCaseString = combinedNames.lower()
 
-lowerName2 = name2.lower()
-countName2 = lowerName2.count()
-loveScore= 0
-if loveScore >= 10 or loveScore <=90:
-    print(f"Your score is {loveScore}, you go together like coke and mentos.")
-elif 40 >= loveScore >= 50:
-    print(f"Your score is {loveScore}, you are alright together.")
+t = lowerCaseString.count("t")
+r = lowerCaseString.count("r")
+u = lowerCaseString.count("u")
+e = lowerCaseString.count("e")
+true = t + r + u + e
+
+l = lowerCaseString.count("l")
+o = lowerCaseString.count("o")
+v = lowerCaseString.count("v")
+e = lowerCaseString.count("e")
+love = l + o + v + e
+
+loveScore = int(str(true) + str(love))
+
+if (loveScore < 10) or (loveScore > 90):
+	print(f"Your score is {loveScore}, you go together like coke and mentos.")
+elif (loveScore >= 40) or (loveScore <= 50):
+	print(f"Your score is {loveScore}, you are alright together.")
 else:
-    print(f"Your score is {loveScore}.")
+	print(f"Your score is {loveScore}.")
